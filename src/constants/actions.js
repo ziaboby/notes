0cc4d91a9c0508435fd3b01e28cc1027f449ac73
Camilla Minutilli <camilla.minutilli@gmail.com>
2021-02-13 21:47:48 +0100

    Added constants

---
+++ src/constants/actions.js
@@ -0,0 +1,3 @@
+export const LOAD_DATA = 'LOAD_DATA'
+export const LOAD_NOTES_FROM_FETCH = 'LOAD_NOTES_FROM_FETCH'
+export const ADD_NOTE = 'ADD_NOTE' 
---
+++ src/constants/common.js
@@ -0,0 +1,4 @@
+export const CURRENT_USER_PICSUM_ID = 669
+export const CURRENT_USER_NAME = 'You'
+export const LOCAL_STORAGE_KEY = 'notes'
+export const DATA_URL = "/data.json"
