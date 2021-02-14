2acb996f2e4d481ba960bbac108d529c9fbed25d
Camilla Minutilli <camilla.minutilli@gmail.com>
2021-02-14 23:12:23 +0100

    Fix to prevent scroll restoration on mobile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './layout/App';
 
+if (((window || {}).history || {}).scrollRestoration) {
+    window.history.scrollRestoration = 'manual';
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
