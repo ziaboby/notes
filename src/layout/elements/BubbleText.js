2b03849692c0a7d85ba2fc2514baf89062b9f85a
Camilla Minutilli <camilla.minutilli@gmail.com>
2021-02-14 20:40:21 +0100

    Added stylesheet

--- src/layout/elements/BubbleText.js
+++ src/layout/elements/BubbleText.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import './BubbleText.scss';
 
 const BubbleText = ({ extraClassNamesSpaceSeparated, children }) => (
-    <div className={"bubble " + (extraClassNamesSpaceSeparated || '')}>
-        <div className="bubble-tail" />
-        <div className="bubble-content">{children}</div>
+    <div className={'bubble ' + (extraClassNamesSpaceSeparated || '')}>
+        <div className='bubble-tail' />
+        <div className='bubble-content'>{children}</div>
     </div>
-)
+);
 
-export default BubbleText
+export default BubbleText;
