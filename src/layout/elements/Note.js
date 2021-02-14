dbd98bbe51e2f2964808966cfe70276323950977
Camilla Minutilli <camilla.minutilli@gmail.com>
2021-02-14 20:39:27 +0100

    Fixed class name

--- src/layout/elements/Note.js
+++ src/layout/elements/Note.js
@@ -30,7 +30,7 @@ const Notes = ({
                 titleText={name}
             />
             <Label extraClassNamesSpaceSeparated={'note-label'}>{name}</Label>
-            <Timestamp extraClassNamesSpaceSeparated={'note-timestammp'}>
+            <Timestamp extraClassNamesSpaceSeparated={'note-timestamp'}>
                 {pubDate + ' - ' + pubTime}
             </Timestamp>
             <BubbleText extraClassNamesSpaceSeparated={'note-bubble'}>{content}</BubbleText>
